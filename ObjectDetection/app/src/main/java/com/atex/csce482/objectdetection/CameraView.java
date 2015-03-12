704c0aa0199d05dcadb3132aa4d8b326926f659f
Chuong Trinh <ctrinh@tamu.edu>
2015-03-12 18:46:50 -0500

    Building CameraView for getting image from camera

---
+++ ObjectDetection/app/src/main/java/com/atex/csce482/objectdetection/CameraView.java
@@ -0,0 +1,35 @@
+package com.atex.csce482.objectdetection;
+
+import android.hardware.Camera;
+import android.view.SurfaceHolder;
+import android.widget.ImageView;
+
+/**
+ * Created by Chuong on 3/12/2015.
+ */
+public class CameraView implements SurfaceHolder.Callback, Camera.PreviewCallback {
+
+
+    public CameraPreview(int layoutWidth, int layoutHeight, ImageView image) {
+
+        
+    }
+
+
+    @Override
+    public void onPreviewFrame(byte[] arg0, Camera arg1)
+    {
+
+
+    }
+
+
+    @Override
+    public void surfaceChanged(SurfaceHolder arg0, int arg1, int arg2, int arg3)
+    {
+
+
+    }
+
+
+}
